6ad4a10f466c18fc1e18cac2163cd8620bd4d45b
shopnest-app <shopnest.application@gmail.com>
2025-08-08 17:05:52 +0530

    Initial commit to service-discovery.

---
+++ src/test/java/com/policy360/service_discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.policy360.service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
